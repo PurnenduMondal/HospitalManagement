1859c78566ccb4599650cf4bc7501971a4f77d96
PurnenduMondal <purnendum@rapidquest.in>
2026-04-08 18:50:14 +0530

    Creating basic infrastructure

---
+++ src/HospitalManagement.Domain/Enums/UserRole.cs
@@ -0,0 +1,9 @@
+namespace HospitalManagement.Domain.Enums;
+
+public static class UserRole
+{
+    public const string Admin   = "Admin";
+    public const string Doctor  = "Doctor";
+    public const string Nurse   = "Nurse";
+    public const string Patient = "Patient";
+}
